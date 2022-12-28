2cb5d7486d4a3a24b176e38714dd6252f1f13d62
Himanshu Gupta <Himanshu.Gupta@crowe.com>
2022-12-28 17:33:10 +0530

    feat: saved sucessfully

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ dotenv.config();
 
 console.log(process.env.MY_NAME);
 console.log(add(1, 3));
+console.log(add(1, 4));
+// var testname="harry";
+//     const helloWorld = () => {
+//         return testname;
+//     }
--- src/math/add.test.ts
+++ src/math/add.test.ts
@@ -1,7 +1,12 @@
 import add from '@src/math/add';
 
+describe('This is a test info', () => {
+  it('should pass', () => {
+    expect(add(4, 2)).toBe(6);
+  });
+});
 describe('This is a test', () => {
   it('should pass', () => {
-    expect(add(1, 2)).toBe(3);
+    expect(add(4, 3)).toBe(7);
   });
 });
