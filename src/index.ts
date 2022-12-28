80e7ac77d3902850ed1ee2112518c6c7c30c0f42
Himanshu Gupta <Himanshu.Gupta@crowe.com>
2022-12-28 19:17:53 +0530

    feat: saved sucessfully

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,3 @@ dotenv.config();
 console.log(process.env.MY_NAME);
 console.log(add(1, 3));
 console.log(add(1, 4));
-// var testname="harry";
-//     const helloWorld = () => {
-//         return testname;
-//     }
