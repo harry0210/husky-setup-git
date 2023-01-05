9b60d0bd3a7fd49b42c4b5ee9387a467024867eb
Himanshu Gupta <Himanshu.Gupta@crowe.com>
2023-01-05 16:39:33 +0530

    test: saved sucessfullydddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd

--- src/math/add.test.ts
+++ src/math/add.test.ts
@@ -2,6 +2,12 @@ import add from '@src/math/add';
 
 describe('This is a test', () => {
   it('should pass', () => {
-    expect(add(4, 3)).toBe(7);
+    expect(add(4, 3)).toBe(8);
+  });
+});
+
+describe('This is a test', () => {
+  it('should pass', () => {
+    expect(add(4, 3)).toBe(8);
   });
 });
