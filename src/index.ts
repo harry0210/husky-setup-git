c9ca6e5470fb0e119a0a4a834b7f394562fdea0c
Himanshu Gupta <Himanshu.Gupta@crowe.com>
2023-01-05 12:11:26 +0530

    feat: saved sucessfully

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import add from '@src/math/add';
 
 console.log(add(1, 3));
 console.log(add(1, 4));
-// var testname="test";
-//     const hello_World = () => {
-//         return 'hello';
-//     }
+const testname = 'test';
+const testdemo = () => {
+  return testname;
+};
+console.log(testdemo);
