bc51de1e116dd62c75c2ad64a766ef7de1100aa8
Himanshu Gupta <Himanshu.Gupta@crowe.com>
2023-01-05 16:25:48 +0530

    test: saved sucessfully

--- src/math/add.test.ts
+++ src/math/add.test.ts
@@ -5,15 +5,3 @@ describe('This is a test', () => {
     expect(add(4, 3)).toBe(7);
   });
 });
-
-describe('This is a test', () => {
-  it('should pass', () => {
-    expect(add(4, 3)).toBe(7);
-  });
-});
-
-describe('This is a test', () => {
-  it('should pass', () => {
-    expect(add(4, 3)).toBe(7);
-  });
-});
